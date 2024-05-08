75bb428b75d319ffe0e64ac9169f9638eb8d64be
linegrr <line.granseth@gmail.com>
2024-05-08 13:08:02 +0200

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,25 @@ export default function App() {
   return (
     <div className="App">
       <Forecast />
+      <footer>
+        This project was coded by
+        <span>
+          <a href="https://github.com/linegrr" target="_blank">
+            Line Granseth
+          </a>
+          and is
+          <a href="https://github.com/linegrr/react-weather" target="_blank">
+            on GitHub
+          </a>
+          and
+          <a
+            href="https://willowy-faloodeh-cfcb68.netlify.app/"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </span>
+      </footer>
     </div>
   );
 }
